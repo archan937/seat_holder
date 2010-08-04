9b3557b4801e1862eae72c6e61feb302babc9929
Paul Engel <paulengel@ArCHanGeL.local>
2010-08-04 21:44:50 +0200

    Released v0.8.4 Also cloning the style attribute

--- releases/0.8.4/seat_holder.js
+++ releases/0.8.4/seat_holder.js
@@ -16,16 +16,16 @@ var scriptHost = (function deriveScriptHost() {
 }());
 
 // *
-// * SeatHolder {version} (Uncompressed)
+// * SeatHolder 0.8.4 (Uncompressed)
 // * The modest Javascript placeholder (used in http://gettopup.com)
 // *
 // * This library requires jQuery (http://jquery.com)
 // *
-// * (c) {year} Paul Engel (Internetbureau Holder B.V.)
+// * (c) 2010 Paul Engel (Internetbureau Holder B.V.)
 // * Except otherwise noted, SeatHolder is licensed under
 // * http://creativecommons.org/licenses/by-sa/3.0
 // *
-// * $Date: {date} $
+// * $Date: 2010-08-04 21:43:16 +0100 (Wed, 04 August 2010) $
 // *
 
 SeatHolder = (function() {
@@ -150,7 +150,7 @@ SeatHolder = (function() {
   };
   
   return {
-    version: "{version}",
+    version: "0.8.4",
     selector: "[seatholder]",
     hintColor: "#AAA",
     init: function() {
